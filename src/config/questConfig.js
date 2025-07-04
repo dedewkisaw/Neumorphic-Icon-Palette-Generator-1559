cd02740fa78ca725179ddb51dab8535456c6cf98
dedewkisaw <166748462+dedewkisaw@users.noreply.github.com>
2025-07-04 10:16:08 +0200

    Delete src directory

--- src/config/questConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  QUEST_FEEDBACK_QUESTID: 'c-greta-feedback-workflow',
-  USER_ID: 'u-06c26fab-a907-4d3c-8aca-08a157727091',
-  APIKEY: 'k-2bb1e855-d25f-4997-9371-e2f47f151198',
-  TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1LTA2YzI2ZmFiLWE5MDctNGQzYy04YWNhLTA4YTE1NzcyNzA5MSIsImlhdCI6MTc1MTQzMjU0MCwiZXhwIjoxNzU0MDI0NTQwfQ.pb6FLbQD7ksWJh77Y8z5EZtFKyKuzosAmnDSfV_FCaE',
-  ENTITYID: 'e-b72c4bf6-3226-45f9-9243-bdd42fe6da82',
-  PRIMARY_COLOR: '#c84bff' // Extracted from your primary-500 color
-};
